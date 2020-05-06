b3ac5a9af237c8f9d669c490d1cd0c4ba173a86a
Jose <jose@plingo.io>
2020-05-06 12:28:09 +0900

    handle error from api

--- src/controllers/api.js
+++ src/controllers/api.js
@@ -1,22 +1,31 @@
 const axios = require('axios');
-
+const get = require('lodash.get');
 const { apiEndpoint, apiToken, apiHost } = require('../config');
 
 const VIDEOS_ENDPOINT = `${apiEndpoint}/videos/youtube`;
 
 const publishVideo = async function (videoData) {
-  const response = await axios({
-    method: 'POST',
-    url: `${apiHost}${VIDEOS_ENDPOINT}`,
-    data: videoData,
-    headers: {
-      Authorization: `Bearer ${apiToken}`
-    }
-  });
+  try {
+    const response = await axios({
+      method: 'POST',
+      url: `${apiHost}${VIDEOS_ENDPOINT}`,
+      data: videoData,
+      headers: {
+        Authorization: `Bearer ${apiToken}`
+      }
+    });
 
-  // TODO handle response status
-  const { data } = response;
-  return data;
+    // TODO handle response status
+    const { data } = response;
+    return data;
+  } catch (err) {
+    const errorData = get(err, 'response.data');
+    if (errorData) {
+      console.log(videoData.youtubeId, err.response.status, err.response.data);
+      return errorData;
+    }
+    throw err;
+  }
 };
 
 module.exports = {
